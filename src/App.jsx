54ad05fa2f905f6df17e3b8ff3111cbe144e7067
sauravsaurav <praj5354@gmail.com>
2023-04-23 20:00:21 +0530

    Optimizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import './App.scss';
-
+import React from 'react';
 import {Routes , Route} from "react-router-dom";
-import Navigation from './routes/Navigation/Navigation.component';
-import CodeEditor from './routes/CodeEditor/CodeEditor';
-import Authentication from './routes/Authentication/Authentication.component';
+import Loader from './components/Loader/Loader.component';
+
+const Navigation = React.lazy(() => import('./routes/Navigation/Navigation.component'));
+const CodeEditor = React.lazy(() => import('./routes/CodeEditor/CodeEditor'));
+const Authentication = React.lazy(() => import('./routes/Authentication/Authentication.component'));
+
+
 const App = () => {
   return (
-    <Routes>
-      <Route element={<Navigation/>}>
-        <Route path='/' index element={<CodeEditor />}></Route>
-        <Route path='/signin' element={<Authentication />}></Route>
-      </Route>
-    </Routes>
+    <React.Suspense fallback={<Loader />}>
+      <Routes>
+        <Route element={<Navigation/>}>
+                <Route path='/' index element={<CodeEditor />}></Route>
+                <Route path='/signin' element={<Authentication />}></Route>
+        </Route>
+      </Routes>
+    </React.Suspense>
   )
 }
 export default App;
---
+++ src/components/Loader/Loader.component.jsx
@@ -0,0 +1,12 @@
+import "./Loader.styles.scss";
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <div className="hourglass"></div>
+        </div>
+    )
+}
+
+
+export default Loader;
